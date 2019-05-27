a66ff3d4d756617b0c751f0d61f8968ff4ed1e10
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-05-27 16:17:56 +0200

    ft_memchr undone

---
+++ ft_memchr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/27 15:28:03 by nmashimb          #+#    #+#             */
+/*   Updated: 2019/05/27 16:17:28 by nmashimb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	str = (char *)s;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return ((void *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+int		main()
+{
+	char a[] = "coding takes hours";
+
+	printf("ft_ %p\n", (char *)ft_memchr(a, '\0', 30));
+	printf("std %p\n", (char *)memchr(a, '\0', 30));
+	printf("str %p\n", &a[18]);
+	return 0;
+}
