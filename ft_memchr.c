c7cb4d81b67284419ccef91388c09a33875ceac3
nmashimb <50702463+nmashimb@users.noreply.github.com>
2019-06-06 18:35:40 +0200

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -16,14 +16,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
-	char	*str;
+	unsigned char	*str;
 
 	i = 0;
-	str = (char *)s;
+	str = (unsigned char *)s;
 	while (i < n)
 	{
 		if (str[i] == (unsigned char)c)
-			return ((void *)&s[i]);
+			return (&s[i]);
 		i++;
 	}
 	return (NULL);
