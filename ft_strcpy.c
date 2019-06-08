72afcf2fa5808348e827680705891fa2156ff423
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-08 11:49:59 +0200

    done

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,21 +6,22 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:08:26 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/20 12:13:08 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/08 11:47:40 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *des, char *src)
+#include "libft.h"
+
+char	*ft_strcpy(char *dst, const char *src)
 {
-	int i;
-	int j;
+	size_t i;
 
 	i = 0;
 	while (src[i] != '\0')
 	{
-		des[i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	des[i] = src[i];
-	return (des);
+	dst[i] = '\0';
+	return (dst);
 }
