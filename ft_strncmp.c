176454b1c2960b8cfc00631db9ade0a035065cea
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-10 11:05:58 +0200

    done

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,20 +6,24 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:04:59 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/22 14:20:31 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/10 10:32:00 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
 
 	i = 0;
-	while (i < n)
+	while (i < n - 1 && s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
+	if (n != 0)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
