b79a93e3ac9c86a4c1c6cc5d9ffdc6dee0e5304e
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-10 16:49:20 +0200

    done

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,35 +6,26 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:28:03 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/27 16:17:28 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/10 16:47:17 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*str;
 
 	i = 0;
+	if (!s && !n)
+		return (NULL);
 	str = (unsigned char *)s;
 	while (i < n)
 	{
 		if (str[i] == (unsigned char)c)
-			return (&s[i]);
+			return ((void *)&s[i]);
 		i++;
 	}
 	return (NULL);
-}
-
-int		main()
-{
-	char a[] = "coding takes hours";
-
-	printf("ft_ %p\n", (char *)ft_memchr(a, '\0', 30));
-	printf("std %p\n", (char *)memchr(a, '\0', 30));
-	printf("str %p\n", &a[18]);
-	return 0;
 }
