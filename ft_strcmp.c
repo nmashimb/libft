b6b52f2b6752025ce23d7fd5f6506b36fca97d25
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-10 11:07:30 +0200

    done

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 13:31:04 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/10 10:19:39 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/10 11:06:58 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	int i;
+	size_t		i;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
