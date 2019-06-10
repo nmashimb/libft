641c54911b6645065a7f020159899645d1cd4c56
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-10 16:16:18 +0200

    done

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,17 +6,21 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 16:28:29 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/24 16:20:56 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/10 16:15:20 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t		i;
 	char		*s_des;
 	const char	*s_src;
 
 	i = 0;
+	if (!src && !dst)
+		return (dst);
 	s_des = (char *)dst;
 	s_src = (const char *)src;
 	while (i < n)
