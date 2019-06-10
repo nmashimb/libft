bbb8c169ae7484f868f2fb7bb73a8215274ae391
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-10 10:04:44 +0200

    done

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 17:15:29 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/08 12:38:34 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/10 10:03:47 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ char	*ft_strrchr(const char *s, int c)
 {
 	size_t len;
 
-	len = ft_strlen(s);
-	while (len != 0 && s != NULL)
+	len = ft_strlen(s) + 1;
+	while (len > 0 && s != NULL)
 	{
+		len--;
 		if (s[len] == (char)c)
 			return ((char *)&s[len]);
-		len--;
 	}
 	return (0);
 }
