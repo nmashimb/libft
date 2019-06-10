fa709ab1f557fcdcd2033142beb1634bfc14ba19
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-10 16:39:29 +0200

    done

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,29 +6,29 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 16:26:41 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/24 23:53:02 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/10 16:39:02 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memccpy(void *restrict dst, const void *restrict src,\
-		int c, size_t n)
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t		i;
+	size_t			i;
 	unsigned char	*s_des;
 	unsigned char	*s_src;
 
 	i = 0;
+	if (!dst && !src)
+		return (dst);
 	s_des = (unsigned char *)dst;
 	s_src = (unsigned char *)src;
-	if (n != 0)
+	while (i < n)
 	{
-		while (i < n)
-		{
-			if (s_src[i] == (unsigned char)c)
-				return (&dst[i + 1]);
-			s_des[i] = s_src[i];
-			i++;
-		}
+		s_des[i] = s_src[i];
+		if (s_src[i] == (unsigned char)c)
+			return (&dst[i + 1]);
+		i++;
 	}
 	return (NULL);
 }
