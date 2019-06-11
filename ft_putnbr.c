5ddeec9986835c0b3521ab4c2831b73959eb906d
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-11 16:51:55 +0200

    done

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,37 +6,28 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 16:48:05 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/06 16:57:51 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/11 16:50:39 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
 void	ft_putnbr(int n)
 {
-	/*if (n == -2147483648) //min value of integers, but doesnt prnt on its own
-		ft_putstr("-2147483648");*/ // change std func to ft_ funct
-	if ((n >= 0 && n <= 9))
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else if ((n >= 0 && n <= 9))
 	{
-		putchar(n + '0');
+		ft_putchar(n + '0');
 	}
-	if (n >= 10)
+	else if (n >= 10)
 	{
 		ft_putnbr(n / 10);
 		ft_putnbr(n % 10);
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
-		putchar('-');
+		ft_putchar('-');
 		ft_putnbr(-n);
 	}
-}
-
-int	main()
-{
-	int a = -2147483648;
-	ft_putnbr(a);
-	return 0;
 }
