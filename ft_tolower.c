42816e75a399aff23365a20a56e59c2d0b0a4e32
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-11 10:19:06 +0200

    done

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,15 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 10:21:28 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/23 10:40:07 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/11 10:18:08 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int		ft_tolower(int c)
 {
-	if ((char)c >= 65 && (char)c <= 90)
+	if (c >= 65 && c <= 90)
 		return (c + 32);
 	return (c);
 }
