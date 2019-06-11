fa7991bd9a2cff5b6461fb9f6f886b576605c2e2
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-11 11:48:06 +0200

    not done

--- ft_memdel.c
+++ ft_memdel.c
@@ -6,46 +6,16 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:21:50 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/31 09:43:22 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/11 11:24:52 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
-/*void	ft_memdel(void **ap)
-{
-	 size_t i;
-
-    i = 0;
-    while (i < sizeof(ap)/sizeof(void *))
-    {
-        free(ap[i]);
-        i++;
-    }
-	ap = NULL;
-}*/
-
-void	ft_memdel(void **ap)
+void	ft_memdel(void **ap) //NOT PROTECTED
 {
 	if (!ap || !*ap)
-	{
 		return ;
-	}
 	free(*ap);
 	*ap = NULL;
-}
-
-int        main()
-{
-    char *s[2] = {"hello, world"};
-   
-	//s[0] =	"hello"; 
-	//s[1] = "world";
-  
-    printf("%p\n", s[0]);
-	//(void **)s;
-	ft_memdel((void **)s);
-	printf("%p\n", s);
-    return 0;
 }
