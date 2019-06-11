919c27a35dddc466d9b6a363c73a25ce16ae3c07
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-11 12:20:36 +0200

    done

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,33 +6,29 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 14:26:11 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/01 14:48:42 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/11 12:19:49 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdlib.h>
-
-char *ft_strmap(char const *s, char (*f)(char))
+#include "libft.h"
+char	*ft_strmap(char const *s, char (*f)(char))
 {
-	size_t i;
-	size_t len;
+	size_t	i;
+	size_t	len;
 	char	*tmp;
 
-	// if (s == NULL)
-	//	return (0);
-	len = 0;
-	while (s[len] != '\0')
-		len++;
+	if (s == NULL)
+		return (0);
 	i = 0;
+	len = ft_strlen(s);
 	tmp = (char *)malloc(len + 1);
 	if (tmp == NULL)
 		return (0);
-	while (s[i] != '\0')
+	while (i < len)
 	{
 		tmp[i] = f(s[i]);
 		i++;
 	}
-	tmp[i] = '\0';
+	tmp[len] = '\0';
 	return (tmp);
 }
