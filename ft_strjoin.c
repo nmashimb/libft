2c322ee2ba698659f637538a489d6068eafeef7d
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-11 14:45:03 +0200

    done

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,26 +6,24 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 15:58:52 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/03 17:05:48 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/11 14:44:44 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
 	size_t	len;
 
-	len = 0;
-	while (*s1++)
-		len++;
-	while (*s2++)
-		len++;
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
 	str = (char *)malloc(len + 1);
-   	while (*s1)
+	if (!str)
+		return (NULL);
+	while (*s1)
 	{
 		*str = *s1;
 		s1++;
@@ -37,24 +35,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		s2++;
 		str++;
 	}
-	str = '\0';
+	*str = '\0';
 	str = str - len;
 	return (str);
-}
-
-int		main()
-{
-//	int len = 0;
-	char *s1 = "abcde";
-	char *str = (char *)malloc(6);
-	while (*s1)
-		{
-			*str = *s1;
-			str++;
-			s1++;
-		}
-	*str = '\0';
-	str = str - 5;	
-	printf("%s\n", str);
-	return 0;
 }
