5d29af0763db664b2a86626e9cfd2fd70e1bf925
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-11 14:09:24 +0200

    done

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,22 +6,24 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 13:00:50 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/03 13:40:16 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/11 14:08:28 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 int		ft_strequ(char const *s1, char const *s2)
 {
 	size_t	i;
+	size_t	len;
 
-	if (s1[0] == '\0' && s2[0] == '\0')
+	if (!s1 && !s2)
 		return (1);
-	if (s1[0] == '\0' && s2[0] != '\0')
+	if (!s1 && s2)
 		return (0);
 	i = 0;
-	while (s1[i] != '\0')
+	len = ft_strlen(s1);
+	while (i < len)
 	{
 		if (s1[i] != s2[i])
 			return (0);
