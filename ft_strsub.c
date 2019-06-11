03d40040d47989ac67e8bcea3834ed513acde190
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-11 14:29:58 +0200

    done

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,46 +6,28 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 15:11:17 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/03 15:55:58 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/11 14:28:07 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "libft.h"
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
-	size_t	j;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	if (s == NULL || len == 0 || i <= start || i < len \
-			|| i - len - 1 == 0 || i - len - 1 <= len)
+	if (!s)
 		return (0);
 	str = (char *)malloc(len + 1);
 	if (str == NULL)
 		return (NULL);
-	j = 0;
-	i = start;
-	while (i < start + len)
+	i = 0;
+	while (i < len)
 	{
-		str[j] = s[start + j];
-		j++;
+		str[i] = s[start + i];
 		i++;
 	}
-	str[j] = '\0';
+	str[i] = '\0';
 	return (str);
-}
-
-int		main()
-{
-	char *s = "abcdefghijklmnopqrstuvwxyz";
-
-	char *x = ft_strsub(s, 7, 9);
-	printf("%s\n", x);	
-	return 0;
 }
