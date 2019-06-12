a81fb8b7fbf31a38ae163c5d9956489fe548815e
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-12 17:50:24 +0200

    done

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,11 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 11:13:21 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/28 11:31:42 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/12 17:49:51 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -20,24 +19,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	if (needle[0] == '\0')
 		return ((char *)haystack);
 	i = 0;
-	while (haystack[i] != '\0')
+	while (haystack[i] != '\0' && (i + ft_strlen(needle) <= len))
 	{
 		if (haystack[i] == needle[0])
 		{
-			if (strncmp(&haystack[i], needle, len) == 0)
+			if (strncmp(&haystack[i], needle, ft_strlen(needle)) == 0)
 				return ((char *)&haystack[i]);
 		}
 		i++;
 	}
 	return (NULL);
 }
-/*
-int main()
-{
-	char *s = "coding consumes days, hours and minutes";
-	char *f = "nutes";
-
-	printf("ft_ %s\n", ft_strnstr(s, f, 3));
-	printf("st_ %s\n", strnstr(s, f, 3));
-	return 0;
-}*/
