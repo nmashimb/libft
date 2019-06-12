f2ebddeb94fc9e05b6bb9b154fdc2c623df82fb4
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-12 13:10:23 +0200

    done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,50 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmashimb <marvin@42.fr>                       +#+  +:+       +#+     */
+/*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/04 10:41:58 by nmashimb             #+#    #+#          */
-/*   Updated: 2019/06/04 11:40:52 by event            ###   ########.fr       */
+/*   Created: 2019/06/12 12:52:54 by nmashimb          #+#    #+#             */
+/*   Updated: 2019/06/12 13:08:20 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
-char	*ft_strtrim(char const *s)
+static size_t	start_ind(char const *s, size_t start)
+{
+	while ((s[start] != '\0') && (s[start] == ' ' || s[start] == '\n'\
+			|| s[start] == '\t'))
+		start++;
+	return (start);
+}
+
+static size_t	end_ind(char const *s, size_t end)
+{
+	while (end >= 0 && (s[end] == ' ' || s[end] == '\n' || s[end] == '\t'))
+		end--;
+	return (end);
+}
+
+char			*ft_strtrim(char const *s)
 {
 	size_t	i;
 	size_t	start;
 	size_t	end;
 	char	*str;
 
-	end = strlen(s); // change me
 	start = 0;
-	while ((s[start] != '\0') && (s[start] == ' ' || s[start] == '\n'	||	s[start] == '\t'))
-		start++;
+	if (!s)
+		return (NULL);
+	if (*s == '\0')
+		return ((char *)s);
+	start = start_ind(s, start);
 	if (s[start] == '\0')
-		return (0);
-	end = end - 1;
-	while (end  >= 0 && (s[end] == ' ' || s[end] == '\n' || s[end] == '\t'))
-		end--;
+		return (ft_strcpy(ft_strnew(1), ""));
+	end = ft_strlen(s) - 1;
+	end = end_ind(s, end);
 	str = (char *)malloc(end - start + 1 + 1);
-	if (str == NULL)
+	if (!str)
 		return (0);
 	i = 0;
 	end = end - start + 1;
-	while (i < end) // USE A FUNCTION TO CPY INSTD
-	{
-		str[i] = s[start];
-		i++;
-		start++;
-	}
-	str[i] = '\0';
+	str = ft_strncpy(str, s + start, end);
+	str[end] = '\0';
 	return (str);
-}
-
-int main()
-{
-	char *s = "";
-	char *x = ft_strtrim(s);
-	printf("%s\n", x);
-	return 0;
 }
