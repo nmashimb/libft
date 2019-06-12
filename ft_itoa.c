18b882b4a9f8d74d443a7038f55d56c4b041095c
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-12 11:22:53 +0200

    done

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,15 +6,35 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 13:21:15 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/05 11:21:49 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/12 11:21:59 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-//static char	*assign_num();
+static char		*assign_num(int end, int i, int n, char *s)
+{
+	while (end <= i)
+	{
+		s[i] = n % 10 + '0';
+		n = n / 10;
+		i--;
+	}
+	return (s);
+}
 
-char	*ft_itoa(int n)
+static int		put_negative(char *str, int n)
+{
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -(n);
+		return (n);
+	}
+	return (n);
+}
+
+char			*ft_itoa(int n)
 {
 	int		i;
 	int		end;
@@ -35,18 +55,10 @@ char	*ft_itoa(int n)
 	str = (char *)malloc(len + 1);
 	if (str == NULL)
 		return (0);
-	if (n < 0)
-	{
-		str[0] = 45;
-		n = (-1) * n;
-	}
-	i = len - 1;
-	while (end <= i)
-	{
-		str[i] = n % 10 + '0';
-		n = n / 10;
-		i--;
-	}
-    str[len] = '\0';
+	if (n == -2147483648)
+		return (ft_strcpy(str, "-2147483648"));
+	n = put_negative(str, n);
+	assign_num(end, len - 1, n, str);
+	str[len] = '\0';
 	return (str);
 }
