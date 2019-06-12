8690b325fe97f2fa0a1262bec949365f8e2688ed
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-12 17:48:46 +0200

    done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,12 +6,13 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 09:36:10 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/05/29 11:31:00 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/12 17:47:20 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <string.h>
-size_t		ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+
+#include "libft.h"
+
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t		i;
 	size_t		dst_len;
@@ -22,32 +23,19 @@ size_t		ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsiz
 	i = 0;
 	des = (char *)dst;
 	sc = (char *)src;
-	dst_len = strlen(des); //DONT FORGET TO USE YOUR OWN STRLEN!!!
-	src_len = strlen(sc);
+	dst_len = ft_strlen(des);
+	src_len = ft_strlen(sc);
 	if (dstsize == 0)
 		return (src_len);
 	else if (dstsize < dst_len)
 		return (dstsize + src_len);
 	else if (dstsize == dst_len)
-		return (dst_len + src_len);	
+		return (dst_len + src_len);
 	while (i < dstsize - dst_len - 1)
 	{
 		des[dst_len + i] = sc[i];
-		i++;	
+		i++;
 	}
 	des[dst_len + i] = '\0';
 	return (dst_len + src_len);
-}
-
-int		main()
-{
-	char ftds[20] = "hellohelloo";
-	char ftsc[] = "world";
-
-	char ds[20] = "hellohelloo";
-	char sc[] = "world";
-
-	printf("ft_ %lu\n", ft_strlcat(ftds, ftsc, 11));
-	printf("std %lu\n", strlcat(ds, sc, 11));
-	return (0);
 }
