63611ab227cb5b0d922ae47509e456c63c4494c5
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-14 15:51:18 +0200

    done

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 11:13:21 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/12 17:49:51 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/14 15:51:02 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	{
 		if (haystack[i] == needle[0])
 		{
-			if (strncmp(&haystack[i], needle, ft_strlen(needle)) == 0)
+			if (ft_strncmp(&haystack[i], needle, ft_strlen(needle)) == 0)
 				return ((char *)&haystack[i]);
 		}
 		i++;
