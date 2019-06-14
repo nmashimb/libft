18ba35394ce97b0e20b23527004e56da2c120ea2
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-14 17:45:55 +0200

    done

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:30:00 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/10 18:09:02 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/14 17:45:16 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,16 @@ int		ft_atoi(const char *str)
 {
 	int		sign;
 	int		i;
-	int		res;
+	long	res;
 
 	i = 0;
 	sign = 1;
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '-')
-	{
 		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
 		i++;
-	}
 	res = 0;
 	while (str[i] != '\0' && (str[i] >= 48 && str[i] <= 57))
 	{
