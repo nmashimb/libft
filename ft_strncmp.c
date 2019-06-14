553d44d3908a50592daf2f7058d77ae35ce490ad
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-14 16:25:55 +0200

    done

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:04:59 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/10 10:32:00 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/14 16:25:06 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while (i < n - 1 && s1[i] != '\0' && s2[i] != '\0')
+	while (i <= ft_strlen(s1) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		{
+			if (ft_isascii(s1[i]) == 1 && ft_isascii(s2[i]) == 1)
+				return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			else
+				return (s2[i] - s1[i]);
+		}
 		i++;
 	}
-	if (n != 0)
-		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
