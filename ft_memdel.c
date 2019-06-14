77454cf91d1a71ed8b9a69a5b84c3a2c2b1718dc
Nyiko MASHIMBYI <nmashimb@c5r8s4.wethinkcode.co.za>
2019-06-14 15:02:33 +0200

    done

--- ft_memdel.c
+++ ft_memdel.c
@@ -6,13 +6,13 @@
 /*   By: nmashimb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:21:50 by nmashimb          #+#    #+#             */
-/*   Updated: 2019/06/11 12:04:56 by nmashimb         ###   ########.fr       */
+/*   Updated: 2019/06/14 15:02:02 by nmashimb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_memdel(void **ap) //NOT PROTECTED
+void	ft_memdel(void **ap)
 {
 	if (!ap)
 		return ;
